ce16272da5c8dc8d8882cf910b4c541011f3e785
kevinduhamelhayes <kevinduhamelh@gmail.com>
2023-12-05 02:39:32 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+          "files": ["*.js", "*.ts", "*.tsx"]
+        }
+      ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "./tsconfig.json"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "import/no-unresolved":"off",
+        "import/extensions": [
+          "off",
+          "ignorePackages",
+          {
+            "js": "never",
+            "jsx": "never",
+            "ts": "never",
+            "tsx": "never"
+          }
+       ],
+    }
+};
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <h1>React Typescript</h1>
+      <h2></h2>
+
+    </>
+  )
+}
+export default App
